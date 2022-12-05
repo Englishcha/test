50398c8251723ea648ca05bcb4e469073109ac55
Englishcha <72689253+Englishcha@users.noreply.github.com>
2022-12-05 17:50:44 +0545

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const User = require("./models/userModel");
 require("dotenv").config();
 const app = express();
-require("./config/db");
+// require("./config/db");
 
 app.get("/", async (req, res) => {
   const user = await User.find({});
